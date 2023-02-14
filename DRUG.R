93c280968cd3fde2014ebb9fd0b3b73f68ced13b
uuihbgg <66097709+uuihbgg@users.noreply.github.com>
2023-02-14 16:20:59 +0800

    Update DRUG.R

--- DRUG.R
+++ DRUG.R
@@ -130,10 +130,74 @@ p <- ggplot(data = drug2, mapping = aes(
 
 
 
+library(STRINGdb)
+library(tidygraph)
+string_db <- STRINGdb$new( version="11.5", species=9606,
+                           score_threshold=100, 
+                           input_directory="../AD/01_getdata")
+fd<-fd_COV[fd_COV$entrezgene_id%in%intersect(gene_id,IRG$ID),]
+genes <- data.frame(ENTREZID = fd$entrezgene_id, 
+                    SYMBOL = rownames(fd))
 
+data_mapped<-genes%>%
+  string_db$map(my_data_frame_id_col_names = "ENTREZID",
+                removeUnmappedRows = TRUE)
+input <- get_input(data_mapped)
+tmp <- graph.data.frame(input, directed = F)
+degree(tmp,normalized = T) %>% .[order(.)]
+net<-get_network(input)
+deg_cut <- 0 #V(net)$deg %>% .[order(.,decreasing = T)] %>% .[30]
+plot_ggraph = ggraph(net,layout = 'centrality',cent=deg)+
+  geom_edge_fan(aes(edge_width=width), color = "lightblue", show.legend = F)+
+  geom_node_point(aes(size=size,color=deg), alpha=0.6)+
+  geom_node_text(aes(filter=deg>deg_cut, size = deg/3,label=name),  
+                 repel = T,family="serif",fontface="bold")+
+  scale_edge_width(range = c(0.2,1))+
+  scale_size_continuous(range = c(2.5,6) )+
+  guides(size="none")+ 
+  scale_color_continuous(name="Degree",low="red",high ="blue",
+                         guide = guide_legend(title.theme = element_text(
+                           size = 15,face = "bold",colour = "red"
+                         ), label.theme = element_text(size = 13,face = "bold"),
+                         direction = "horizontal",title.position = "top",
+                         title.vjust = 0.5))+ 
+  theme_graph()+
+  theme(legend.position=c(0.35,0))
+save(input,net,file = "Rdatas/input.Rdata")
 
+load(file = "C:/Users/LWH/Downloads/bioinfo/AD/00_drugbank/drug_link.Rdata")
+uniprot = drug_targets_polypep_ex_ident %>% .[.$resource == "UniProtKB",]
+rownames(uniprot) = uniprot$parent_key
+drug_targets = drug_targets %>% .[.$id %in% uniprot$parent_key,]
+ensembl = biomaRt::useDataset("hsapiens_gene_ensembl",
+                              mart=biomaRt::useMart("ensembl"))
+gene_targets = biomaRt::getBM(attributes=c('uniprotswissprot','entrezgene_id'),
+                              filters = "uniprotswissprot",
+                              values = uniprot$identifier,
+                              mart = ensembl)
+gene_targets = gene_targets[!is.na(gene_targets$entrezgene_id),]
 
-
+x$DTGs<-fd_COV[fd_COV$entrezgene_id%in%gene_targets$entrezgene_id,]$hgnc_symbol
+intersect(x$DTGs,x$IRGs) %>% intersect(x$DEGs_COVID) %>% intersect(x$DEGs_IS)
+library(VennDiagram)
+library("ggVennDiagram")
+display_venn <- function(x, ...){
+  library(VennDiagram)
+  grid.newpage()
+  venn_object <- venn.diagram(x, filename = NULL, ...)
+  grid.draw(venn_object)
+}
+venn<-display_venn(
+  x,
+  # category.names = c("Set 1" , "Set 2 " , "Set 3", "Set 4"),
+  # Circles
+  lwd=2,lty='blank',fill=c("#999999","#E69F00","#56B4E9","#009E73"),
+  # Numbers
+  cex=1,fontface="bold.italic",
+  # Set names
+  cat.cex=1.2,cat.fontface="bold",cat.default.pos="outer",
+  cat.dist = c(0.055, 0.055, 0.1, 0.1)
+)
 
 
 
